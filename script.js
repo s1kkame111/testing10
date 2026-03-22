8d10690489fe5e9b34ab50cdf25b5f6844474479
s1kkame111 <xmelan1114tt@gmail.com>
2026-03-22 12:46:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -323,11 +323,7 @@ function step() {
 }
 
 async function loop() {
-  await animate(".indicator", 2, { x: 0 });
-  await animate(".indicator", 0.8, { x: window.innerWidth, delay: 0.3 });
-  set(".indicator", { x: -window.innerWidth });
-  await step();
-  loop();
+  // disabled
 }
 
 async function loadImage(src) {
